98cba963d872dde7897d69e215428b7e7941dec8
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-08-18 11:31:49 -0700

    Tambahkan Floating action button refresh

--- app/src/main/java/id/andaglos/jadwaldosenunila/MainActivity.java
+++ app/src/main/java/id/andaglos/jadwaldosenunila/MainActivity.java
@@ -1,27 +1,45 @@
 package id.andaglos.jadwaldosenunila;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    FloatingActionButton button;
     WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_main);
+
+        // Makes Progress bar Visible
+        getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+
 
 
         //String url = "http://www.kinerja.com";
          web = (WebView)findViewById(R.id.webViewApp);
         web.getSettings().setJavaScriptEnabled(true);
+        web.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                //Make the bar disappear after URL is loaded, and changes string to Loading...
+                setTitle("Loading...");
+                setProgress(progress * 100); //Make the bar disappear after URL is loaded
 
+                // Return the app name after finish loading
+                if(progress == 100)
+                    setTitle(R.string.app_name);
+            }
+        });
 
         web.setWebViewClient(new WebViewClient(){
 
@@ -33,11 +51,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
         web.loadUrl("http://aac1945.com/jadwal-dosen/public");
         //web.loadUrl(url)
 
 
-        Button button = (Button) findViewById(R.id.button);
+         button = (FloatingActionButton) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
